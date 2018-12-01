c4078c39ae4590a492d78a56952b725ee88f60ee
Aart Goossens <aart@goossens.me>
2018-12-01 10:44:03 +0100

    Fixes license (MIT)

--- opendata-python/setup.py
+++ opendata-python/setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: Apache Software License 2.0",
+        'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
     ],
 )
